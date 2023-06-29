105cd78a6fa1f367357168a088429efd22f996d6
yolovi <yolandalopevi@gmail.com>
2023-06-29 21:20:36 +0200

    endpoint para relacionar productos con categorias en la carpeta intermedia

--- index.js
+++ index.js
@@ -3,18 +3,23 @@ const app = express();
 const PORT = 3000;
 const mysql = require("mysql2");
 
-//MIDDLEWARES
-app.use(express.json());
-
-//ENDPOINTS
-
+// configuramos la conexion con la base de datos
 const db = mysql.createConnection({
   host: "localhost",
   user: "root",
   password: "mySql.1234",
-  database: "expressDB",
+  database: "expressDB", // este punto se hace despues de crear la base de datos (el primer endpoint)
 });
+
+
+//establecemos la conexion con la base de datos
 db.connect();
+
+//MIDDLEWARES
+app.use(express.json());
+
+
+//ENDPOINTS
 
 //Crear la base de datos
 app.get("/createdb", (req, res) => {
@@ -111,7 +116,7 @@ app.get("/products", (req, res) => {
   });
 });
 
-// endpoint para mostrar todos los productos
+// endpoint para mostrar todas las categorias
 app.get("/categories", (req, res) => {
     let sql = "SELECT * FROM categories";
     db.query(sql, (err, result) => {
@@ -119,6 +124,29 @@ app.get("/categories", (req, res) => {
       res.send(result);
     });
   });
+
+//--------------------------------------------------------------
+
+//Ruta para insertar una relación en la tabla intermedia cat_prod
+app.post('/cat_prod', (req, res) => {
+  const { productId, categoryId } = req.body;
+
+  const sql = 'INSERT INTO cat_prod (product_id, category_id) VALUES (?, ?)'; // el ? es un marcador de posicion 
+  const values = [productId, categoryId]; //los valores reales 
+
+  db.query(sql, values, (err, result) => {
+    if (err) {
+      console.error('Error al ejecutar la consulta: ', err);
+      res.status(500).send('Error en el servidor');
+      return;
+    }
+    res.send({msg:'Relación insertada correctamente ', result});
+  });
+});
+
+//Los valores reales que se insertarán se representan mediante marcadores de posición ?. Los marcadores de posición se utilizan para evitar problemas de seguridad como la inyección de SQL y también permiten reutilizar la consulta.
+
+
 
 // enpoint para mostrar todos los products con sus categorias
 app.get("/cat_prod", (req, res) => {
